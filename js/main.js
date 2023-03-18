0746e2fc432875fabf42ceb3eef254e9be8ed13a
Camilo Figueroa <cigotete@gmail.com>
2023-03-18 14:47:12 -0500

    Passing parameters to Vue component.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@ const app = Vue.createApp({
   data() {
     return {
       message: "Hello Vue!",
-      count: 5
+      count: 5,
+      items: [
+        {
+          id: 1,
+          name: 'Lorem ipsum dolor sit amet, qui minim labore',
+          price: 10
+        },
+        {
+          id: 2,
+          name: 'Adipisicing minim sint cillum sint consectetur cupidatat',
+          price: 12
+        },
+        {
+          id: 3,
+          name: 'Minim labore adipisicing minim sint cillum sint consectetur cupidatat',
+          price: 6
+        }
+      ]
     }
   }
 });
@@ -19,5 +36,17 @@ app.component('button-counter', {
     <button @click="count++">
       You have made click {{ count }} times.
     </button>
+  `
+});
+
+
+app.component('detail-items', {
+  data() {
+    return {}
+  },
+  props: ['item'],
+  template: `
+    <h1>Item: {{item.name}}</h1>
+    <p>Price: {{'$' + item.price}}</p>
   `
 });
