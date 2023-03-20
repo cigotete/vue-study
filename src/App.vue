cc1136c5c69750db0b8f5a86f10da21b5d930486
Camilo Figueroa <cigotete@gmail.com>
2023-03-19 22:50:27 -0500

    Created a new component and called it from main App.vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Title outside CounterTimes component</h1>
+  <counter-times />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterTimes from './components/CounterTimes'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterTimes
   }
 }
 </script>
---
+++ src/components/CounterTimes.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Hello from CounterTimes component</h1>
+
+  <button @click="decreaseCounter()">-</button>
+  {{ counter }}
+  <button @click="increaseCounter()">+</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: grey;
+  }
+</style>
